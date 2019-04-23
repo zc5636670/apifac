27612766c714cfa651990a4ec4ea6dc16d0354de
ChaosDove <609634874@qq.com>
2019-04-23 23:05:19 +0800

    FIRST

---
+++ client.py
@@ -0,0 +1,102 @@
+# author Chaos
+import os
+import json
+
+import requests
+
+__all__ = ['ApiFacError', 'GetClient', "PostClient", "ApiClient"]
+
+
+class ApiFacError(Exception):
+    def __init__(self, err_msg, status_code):
+        self.err_msg = err_msg
+        self.code = status_code
+
+
+class MethodClient(object):
+    def __init__(self, name):
+        self.name = name
+        self.host = None
+        self.port = None
+        self.url_prefix = None
+        self.use_ssl = False
+
+    def init_host_port(self, host, port, url_prefix, use_ssl=False):
+        self.host = host
+        self.port = port
+        self.url_prefix = url_prefix
+        self.use_ssl = use_ssl
+
+    @property
+    def url(self):
+        if self.use_ssl:
+            http = 'https://'
+        else:
+            http = 'http://'
+        url = "{}{}:{}{}".format(http, self.host, self.port, os.path.join('/', self.url_prefix, self.name))
+        return url
+
+    def _request(self, params={}, data={}):
+        raise NotImplemented
+
+    def __call__(self, params={}, data={}):
+        try:
+            print(self.url)
+            result = self._request(params=params, json=data)
+            if not result.status_code == 200:
+                raise ApiFacError(result.content, result.status_code)
+            return json.loads(result.text)
+        except ApiFacError as e:
+            raise e
+        except Exception as e:
+            raise ApiFacError("requests报错：{}".format(str(e)), 599)
+
+
+class GetClient(MethodClient):
+    def _request(self, params={}, json={}):
+        return requests.get(self.url, params=params, data=json)
+
+
+class PostClient(MethodClient):
+    def _request(self, params={}, json={}):
+        return requests.post(self.url, params=params, data=json)
+
+
+class ApiClientMeta(type):
+    def __new__(mcs, name, bases, attrs):
+        # 下面的名字一定要和api的基类名称一致
+        if name == 'ApiClient':
+            return type.__new__(mcs, name, bases, attrs)
+        mappings = dict()
+        for k, v in attrs.items():
+            if issubclass(type(v), MethodClient):
+                mappings[k] = v
+        attrs['apis'] = mappings
+        # for k in mappings.keys():
+        #     attrs.pop(k)
+        return type.__new__(mcs, name, bases, attrs)
+
+
+class ApiClient(object, metaclass=ApiClientMeta):
+    def __init__(self, host, port, url_prefix='/', use_ssl=False):
+        self.host = host
+        self.port = port
+        self.url_prefix = url_prefix
+        self.use_ssl = use_ssl
+        self.init_host_port()
+
+    def init_host_port(self):
+        for k, v in self.apis.items():
+            v.init_host_port(self.host, self.port, self.url_prefix, use_ssl=self.use_ssl)
+
+
+#  ####### 以下为测试代码$##########3
+class HelloClient(ApiClient):
+    hello = GetClient('hello')
+    post = PostClient('post')
+
+
+if __name__ == '__main__':
+    hello_client = HelloClient(host='127.0.0.1', port=15000, url_prefix='api')
+    res = hello_client.hello(params={'name': 'chaos'})
+    print(res)
---
+++ server.py
@@ -0,0 +1,99 @@
+# author Chaos
+import os
+
+from flask import jsonify
+from flask import Flask
+from flask import request
+
+__all__ = ['GetServer', 'PostServer', 'ApiServer']
+
+
+class RequestMethod(object):
+    def __init__(self, url_rule, func, method_type):
+        self.url_rule = url_rule
+        self.func = func
+        self.method_type = method_type
+
+    def __repr__(self):
+        return '<%s:%s>' % (self.__class__.__name__, self.name)
+
+    def response(self, *args, **kwargs):
+        content, code = '', 200
+        try:
+            content = self.func(*args, **kwargs)
+            if isinstance(content, str):
+                return content, code
+            if isinstance(content, dict) or isinstance(content, list):
+                return jsonify(content), 200
+        except Exception as e:
+            result = 'error'
+            return jsonify(dict(err_msg=result)), 500
+
+
+class GetServer(RequestMethod):
+    def __init__(self, name, func):
+        super(GetServer, self).__init__(name, func, 'GET')
+
+
+class PostServer(RequestMethod):
+    def __init__(self, name, func):
+        super(PostServer, self).__init__(name, func, 'POST')
+
+
+class ApiServerMeta(type):
+    def __new__(mcs, name, bases, attrs):
+        if name == 'ApiServer':
+            return type.__new__(mcs, name, bases, attrs)
+        mappings = dict()
+        for k, v in attrs.items():
+            if issubclass(type(v), RequestMethod):
+                mappings[k] = v
+        attrs['urls'] = mappings
+        for k in mappings.keys():
+            attrs.pop(k)
+        return type.__new__(mcs, name, bases, attrs)
+
+
+class ApiServer(object, metaclass=ApiServerMeta):
+    def __init__(self, name, url_prefix="", flask_name=__name__):
+        self.name = name
+        self.url_prefix = url_prefix
+        self.app = Flask(flask_name)
+        self.init_urls()
+
+    def init_urls(self):
+        for k, v in self.urls.items():
+            self.app.add_url_rule(os.path.join('/', self.url_prefix, v.url_rule),
+                                  view_func=v.response,
+                                  endpoint="%s.%s" % (self.name, k),
+                                  methods=[v.method_type])
+            print(os.path.join('/', self.url_prefix, v.url_rule))
+
+    def export_json(self):
+        pass
+
+    def export_python(self):
+        pass
+
+
+#  ####### 以下为测试代码$##########3
+def hello_word():
+    name = request.args.get('name', None)
+    return {"content": 'Hello {}!'.format(name)}
+
+
+def post_data():
+    data = request.json.get('html', '')
+    return {}
+
+
+class ApiTestServer(ApiServer):
+    hello = GetServer('hello', hello_word)
+    post_data = PostServer('post', post_data)
+
+
+api = ApiTestServer('test', url_prefix="api")
+app = api.app
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=15000)
